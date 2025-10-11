bf27bd212a20bf88a05a2295a6f1d26375baf357
Jessica Czechumska <jczechumska@edu.cdv.pl>
2025-10-11 10:42:00 +0200

    Pierwsze zadania 1-3

--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -1,3 +1,64 @@
 ﻿// See https://aka.ms/new-console-template for more information
+/*
+// Example 1
+const int requiredAge = 18;
+const string accessDeniedMessage = "You must be 18 years old.";
+const string accessAllowedMessage = "Welcome to my shop!";
 
-Console.WriteLine("Hello, World!");
+int age = 16;
+
+while (age < requiredAge) {
+
+Console.Write("Podaj swój wiek: ");
+var userInput = Console.ReadLine();
+
+age = Convert.ToInt32(userInput);
+
+if (age < requiredAge)
+{
+    Console.WriteLine(accessDeniedMessage);
+}
+else
+{
+    Console.WriteLine(accessAllowedMessage);
+}}
+*/
+// Example 2
+/*
+string [] names = {"John", "Jane", "Joe", "Alice", "Artur"};
+
+Console.WriteLine("Names: ");
+
+for (int i = 0; i < names.Length; i++)
+{
+    Console.WriteLine(names[i]);
+}
+
+foreach (string name in names)
+{
+    Console.WriteLine(name);
+}*/
+
+
+// Zad 1
+/*
+const string correct_password = "admin123";
+string user_password = "admin12";
+
+while (user_password != correct_password)
+{
+    Console.WriteLine("Podaj hasło: ");
+    user_password = Console.ReadLine();
+}*/
+
+// Zad 2
+
+int liczba = 0;
+
+do
+{
+    Console.Write("Podaj liczbę większą od 0: ");
+    liczba = Convert.ToInt32(Console.ReadLine());
+}
+while (liczba < 0);
+Console.WriteLine("Poprawna liczba!");
