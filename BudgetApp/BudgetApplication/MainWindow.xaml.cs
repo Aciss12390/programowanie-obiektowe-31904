d0cd98e746da050baa96effa6b25d492490f4852
Aciss12390 <jessicaczechumska@gmail.com>
2026-01-17 09:00:35 +0100

    Checkpoint przed sprzątaniem duplikatów (CS0436)

---
+++ BudgetApp/BudgetApplication/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using BudzetDomowy.Services;
+using BudzetDomowy.ViewModels;
+
+namespace BudgetApplication
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var manager = new BudgetManager();
+            DataContext = new TransactionsViewModel(manager);
+        }
+    }
+}
