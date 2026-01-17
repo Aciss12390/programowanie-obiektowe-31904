251831f34cb2ac4eff6119b575086463710808cf
Aciss12390 <jessicaczechumska@gmail.com>
2026-01-17 10:46:19 +0100

    Naprawiam podpięcie konwertera nazw kategorii (namespace/assembly)

---
+++ BudgetApp/BudgetApplication/Converters/CategoryIdToNameConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Data;
+using BudzetDomowy.Models;
+
+namespace BudgetApplication.Converters
+{
+    public class CategoryIdToNameConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values == null || values.Length < 2)
+                return "";
+
+            if (values[0] is not int categoryId)
+                return "";
+
+            if (values[1] is not System.Collections.IEnumerable categoriesEnumerable)
+                return categoryId.ToString();
+
+            var category = categoriesEnumerable.Cast<Category>().FirstOrDefault(c => c.Id == categoryId);
+            return category?.Name ?? categoryId.ToString();
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            // Nie edytujemy tego w tabeli
+            return new object[] { Binding.DoNothing, Binding.DoNothing };
+        }
+    }
+}
